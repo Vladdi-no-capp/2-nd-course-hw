c1f65573b8ae05c7ad26f68200d3ff287b47a350
Vladdi-no-capp <Vbelka77@gmail.com>
2026-06-01 00:21:01 +0900

    add new hw5 and chenged index

---
+++ JS HW/hw5.js
@@ -0,0 +1,145 @@
+function simpleArithmeticGame() {
+    const firstNumber = Math.floor(Math.random() * 10) + 1;
+    const secondNumber = Math.floor(Math.random() * 10) + 1;
+    const operations = ['+', '-', '*'];
+    const operation = operations[Math.floor(Math.random() * operations.length)];
+    let correctAnswer;
+
+    if (operation === '+') {
+        correctAnswer = firstNumber + secondNumber;
+    } else if (operation === '-') {
+        correctAnswer = firstNumber - secondNumber;
+    } else {
+        correctAnswer = firstNumber * secondNumber;
+    }
+
+    const userAnswer = Number(prompt(`${firstNumber} ${operation} ${secondNumber} = ?`));
+
+    if (userAnswer === correctAnswer) {
+        alert('Правильно!');
+    } else {
+        alert(`Неправильно. Правильный ответ: ${correctAnswer}`);
+    }
+}
+
+// Задание 1
+const task1Array = [1, 5, 4, 10, 0, 3];
+
+for (let i = 0; i < task1Array.length; i++) {
+    console.log(task1Array[i]);
+
+    if (task1Array[i] === 10) {
+        break;
+    }
+}
+
+// Задание 2
+const task2Array = [1, 5, 4, 10, 0, 3];
+console.log(task2Array.indexOf(4));
+
+// Задание 3
+const task3Array = [1, 3, 5, 10, 20];
+console.log(task3Array.join(' '));
+
+// Задание 4
+const task4Array = [];
+
+for (let i = 0; i < 3; i++) {
+    const nestedArray = [];
+
+    for (let j = 0; j < 3; j++) {
+        nestedArray.push(1);
+    }
+
+    task4Array.push(nestedArray);
+}
+
+console.log(task4Array);
+
+// Задание 5
+const task5Array = [1, 1, 1];
+task5Array.push(2, 2, 2);
+console.log(task5Array);
+
+// Задание 6
+const task6Array = [9, 8, 7, 'a', 6, 5];
+task6Array.sort();
+task6Array.pop();
+console.log(task6Array);
+
+// Задание 7
+function guessArrayNumber() {
+    const task7Array = [9, 8, 7, 6, 5];
+    const userNumber = Number(prompt('Угадайте число'));
+
+    if (task7Array.includes(userNumber)) {
+        alert('Угадал');
+    } else {
+        alert('Не угадал');
+    }
+}
+
+// Задание 8
+const task8String = 'abcdef';
+console.log(task8String.split('').reverse().join(''));
+
+// Задание 9
+const task9Array = [[1, 2, 3], [4, 5, 6]];
+console.log(task9Array.flat());
+
+// Задание 10
+const task10Array = [2, 5, 7, 1, 10, 4];
+
+for (let i = 0; i < task10Array.length - 1; i++) {
+    console.log(task10Array[i] + task10Array[i + 1]);
+}
+
+// Задание 11
+function getSquares(numbers) {
+    return numbers.map((number) => number ** 2);
+}
+
+console.log(getSquares([1, 2, 3, 4]));
+
+// Задание 12
+function getWordLengths(words) {
+    return words.map((word) => word.length);
+}
+
+console.log(getWordLengths(['apple', 'cat', 'javascript']));
+
+// Задание 13
+function getNegativeNumbers(numbers) {
+    return numbers.filter((number) => number < 0);
+}
+
+console.log(getNegativeNumbers([1, -2, 3, -4, 5, -6]));
+
+// Задание 14
+const task14Array = [];
+const evenNumbers = [];
+
+for (let i = 0; i < 10; i++) {
+    const randomNumber = Math.round(Math.random() * 10);
+    task14Array.push(randomNumber);
+
+    if (randomNumber % 2 === 0) {
+        evenNumbers.push(randomNumber);
+    }
+}
+
+console.log(task14Array);
+console.log(evenNumbers);
+
+// Задание 15
+const task15Array = [];
+let sum = 0;
+
+for (let i = 0; i < 6; i++) {
+    const randomNumber = Math.floor(Math.random() * 10) + 1;
+    task15Array.push(randomNumber);
+    sum += randomNumber;
+}
+
+console.log(task15Array);
+console.log(sum / task15Array.length);
